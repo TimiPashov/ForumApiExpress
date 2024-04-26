72457f0738bdfc194fd893e1f8c7d00d89010f1c
TimiPashov <pashov91@gmail.com>
2024-04-26 14:41:22 +0300

    add new allowed cors url

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
     development: {
         port: process.env.PORT || 3000,
         dbURL: 'mongodb+srv://pashov91:Tim911003@mynewcluster.0hywvsh.mongodb.net/Forum',
-        origin: ['http://localhost:5555', 'http://localhost:4200']
+        origin: ['http://localhost:5555', 'http://localhost:4200', 'https://myforumjs.web.app']
     },
     production: {
         port: process.env.PORT || 3000,
