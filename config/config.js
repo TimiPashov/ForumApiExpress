f2f1d6fcb129ef71e67e957f7ce0cfb2f99dc28d
TimiPashov <pashov91@gmail.com>
2024-04-26 15:04:44 +0300

    add cors url

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ const config = {
     production: {
         port: process.env.PORT || 3000,
         dbURL: 'mongodb+srv://pashov91:Tim911003@mynewcluster.0hywvsh.mongodb.net/Forum',
-        origin: ['https://myforumjs.web.app']
+        origin: ['https://myforumjs.web.app', 'http://localhost:4200']
     }
 };
 
